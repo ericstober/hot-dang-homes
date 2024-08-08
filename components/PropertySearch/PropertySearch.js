6cb566a9255b0acff7df42cb1d937d03fc2cf29e
ericstober <erictstober@gmail.com>
2024-08-08 14:02:11 -0500

    render property search results

--- components/PropertySearch/PropertySearch.js
+++ components/PropertySearch/PropertySearch.js
@@ -1,14 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Results } from "./Results";
 
 export const PropertySearch = () => {
+  const [properties, setProperties] = useState([]);
+
   useEffect(() => {
     const search = async () => {
       const response = await fetch(`/api/search`);
       const data = await response.json();
       console.log("SEARCH DATA: ", data);
+      setProperties(data.properties);
     };
     search();
   }, []);
 
-  return <div>property search</div>;
+  return (
+    <div>
+      <Results properties={properties} />
+    </div>
+  );
 };
